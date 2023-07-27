0da22b40a6b2e69e1e4efc298e3d6172fb82e1e3
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-07-27 21:20:16 +0200

    age calculation implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,45 @@ import InputForm from '@/components/InputForm';
 export default function Home() {
   const [age, setAge] = useState({ days: '--', months: '--', years: '--' });
 
-  const submitHandler = (
-    day: number,
-    month: number,
-    year: number,
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
-    setAge({
-      days: String(day),
-      months: String(month),
-      years: String(year),
-    });
+  const submitHandler = (day: number, month: number, year: number) => {
     const now = new Date();
-    console.log(now, day, month, year);
+    const then = new Date(year, month - 1, day);
+
+    let isLeap = false;
+    const currYear = now.getFullYear();
+    if (currYear % 4 === 0) {
+      if (currYear % 100 === 0) {
+        if (currYear % 400 === 0) isLeap = true;
+      } else {
+        isLeap = true;
+      }
+    }
+
+    const prevMonth = now.getMonth() - 1;
+    let plusDays = 0;
+    if ([1, 3, 5, 7, 8, 10, 12].includes(prevMonth)) plusDays = 31;
+    else if ([4, 6, 9, 11].includes(prevMonth)) plusDays = 30;
+    else plusDays = isLeap ? 29 : 28;
+
+    let years = now.getFullYear() - then.getFullYear();
+
+    let months = now.getMonth() - then.getMonth();
+    if (months < 0) {
+      months += 12;
+      years--;
+    }
+
+    let days = now.getDate() - then.getDate();
+    if (days < 0) {
+      days += plusDays;
+      months--;
+    }
+
+    setAge({
+      days: String(days),
+      months: String(months),
+      years: String(years),
+    });
   };
 
   return (
@@ -29,13 +54,13 @@ export default function Home() {
 
       <div id="display" className="flex flex-col gap-2">
         <h1 className="text-6xl italic font-extrabold">
-          <span className="text-purple">{age.years}</span>years
+          <span className="text-purple">{age.years}</span> years
         </h1>
         <h1 className="text-6xl italic font-extrabold">
-          <span className="text-purple">{age.months}</span>months
+          <span className="text-purple">{age.months}</span> months
         </h1>
         <h1 className="text-6xl italic font-extrabold">
-          <span className="text-purple">{age.days}</span>days
+          <span className="text-purple">{age.days}</span> days
         </h1>
       </div>
     </main>
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -5,53 +5,87 @@ import Image from 'next/image';
 import arrowImg from '../assets/images/icon-arrow.svg';
 
 interface ChildProps {
-  onCalcAge: (
-    day: number,
-    month: number,
-    year: number,
-    e: React.FormEvent<HTMLFormElement>
-  ) => void;
+  onCalcAge: (day: number, month: number, year: number) => void;
   children?: React.ReactNode;
 }
 
 const InputForm: React.FC<ChildProps> = ({ onCalcAge }) => {
-  const [day, setDay] = useState(19);
-  const [month, setMonth] = useState(9);
-  const [year, setYear] = useState(1984);
+  const [input, setInput] = useState<Record<string, string>>({
+    day: '',
+    month: '',
+    year: '',
+  });
+
+  const [isValid, setIsValid] = useState<Record<string, boolean>>({
+    day: true,
+    month: true,
+    year: true,
+  });
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const field = e.target.name;
+    setInput({ ...input, [field]: e.target.value });
+  };
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { day, month, year } = input;
+
+    if (day && month && year) {
+      onCalcAge(Number(day), Number(month), Number(year));
+      setInput({
+        day: '',
+        month: '',
+        year: '',
+      });
+    }
+  };
 
   return (
     <form
-      className="flex w-screen gap-5 px-8 justify-center"
-      onSubmit={onCalcAge.bind(null, day, month, year)}
+      className="flex flex-col w-screen gap-10 px-8 justify-center items-center"
+      onSubmit={submitHandler}
     >
-      <label className="text-xs tracking-xtrawide text-smokeygrey flex flex-col font-bold flex-1 gap-1">
-        DAY
-        <input
-          type="text"
-          className="w-full text-black text-xl w-21 p-3 border border-lightgrey rounded-xl"
-          value={day}
-          onChange={(e) => setDay(Number(e.target.value))}
-        />
-      </label>
-      <label className="text-xs tracking-xtrawide text-smokeygrey flex flex-col font-bold flex-1 gap-1">
-        MONTH
-        <input
-          type="text"
-          className="w-full text-black text-xl w-21 p-3 border border-lightgrey rounded-xl"
-          value={month}
-          onChange={(e) => setMonth(Number(e.target.value))}
-        />
-      </label>
-      <label className="text-xs tracking-xtrawide text-smokeygrey flex flex-col font-bold flex-1 gap-1">
-        YEAR
-        <input
-          type="text"
-          className="w-full text-black text-xl w-21 p-3 border border-lightgrey rounded-xl"
-          value={year}
-          onChange={(e) => setYear(Number(e.target.value))}
-        />
-      </label>
-      <button type="submit" className="p-5 bg-purple rounded-full">
+      <div className="flex w-screen gap-5 px-8 justify-center">
+        <label className="text-xs tracking-xtrawide text-smokeygrey flex flex-col font-bold flex-1 gap-1">
+          DAY
+          <input
+            type="text"
+            className="w-full text-black text-xl w-21 p-3 border border-lightgrey rounded-xl focus:outline-purple"
+            placeholder="DD"
+            name="day"
+            value={input.day}
+            onChange={inputHandler}
+          />
+          {isValid.day && <p className="">This Field is required.</p>}
+        </label>
+        <label className="text-xs tracking-xtrawide text-smokeygrey flex flex-col font-bold flex-1 gap-1">
+          MONTH
+          <input
+            type="text"
+            className="w-full text-black text-xl w-21 p-3 border border-lightgrey rounded-xl focus:outline-purple"
+            name="month"
+            placeholder="MM"
+            value={input.month}
+            onChange={inputHandler}
+          />
+        </label>
+        <label className="text-xs tracking-xtrawide text-smokeygrey flex flex-col font-bold flex-1 gap-1">
+          YEAR
+          <input
+            type="text"
+            className="w-full text-black text-xl w-21 p-3 border border-lightgrey rounded-xl focus:outline-purple"
+            name="year"
+            placeholder="YYYY"
+            value={input.year}
+            onChange={inputHandler}
+          />
+        </label>
+      </div>
+      <button
+        type="submit"
+        className="p-5 bg-purple rounded-full hover:bg-black active:bg-black"
+      >
         <div className="relative h-7 w-7">
           <Image
             src={arrowImg}
